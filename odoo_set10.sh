d83587fb8e615782a069e5229c04051939933b8b
Bessam Djerad <bessam.dj@gmail.com>
2017-06-01 10:37:20 +0100

    Update odoo_set10.sh

--- odoo_set10.sh
+++ odoo_set10.sh
@@ -30,70 +30,14 @@ OE_CONFIG="$OE_USER-server"
 # Install PostgreSQL Server
 #--------------------------------------------------
 
-echo -e "\n---- Install PostgreSQL Server ----"
-# sudo apt-get install postgresql -y
-
-echo -e "\n---- PostgreSQL $PG_VERSION Settings  ----"
-sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/postgresql/9.6/main/postgresql.conf
-
-echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
-
-sudo service postgresql restart
-
-#--------------------------------------------------
-# System Settings
-#--------------------------------------------------
-
-echo -e "\n---- Create ODOO system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
-
-echo -e "\n---- Create Log directory ----"
-sudo mkdir /var/log/$OE_USER
-sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
-
-#--------------------------------------------------
-# Install Basic Dependencies
-#--------------------------------------------------
-echo -e "\n---- Install tool packages ----"
-sudo apt-get install wget subversion git zlib1g-dev python-pip python-imaging python-setuptools python-dev python-pychart python-unittest2 python-zsi python-webdav python-simplejson python-pybabel python-libxslt1 libxslt-dev libxml2-dev libldap2-dev libsasl2-dev node-less postgresql-server-dev-all bzr bzrtools gdebi-core -y
-sudo pip install -r /odoo/doc/requirements.txt
-sudo pip install -r /odoo/requirements.txt
-
-sudo apt-get install wget git python-pip gdebi-core -y
-	
-echo -e "\n---- Install python packages ----"
-sudo apt-get install python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil -y python-suds
-	
-echo -e "\n---- Install python libraries ----"
-sudo pip install gdata psycogreen ofxparse XlsxWriter
-
-echo -e "\n--- Install other required packages"
-sudo apt-get install node-clean-css -y
-sudo apt-get install node-less -y
-sudo apt-get install python-gevent -y
 
 echo -e "\n---- Install wkhtml and place on correct place for ODOO 8-9-10 ----"
-sudo wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+sudo wget http://downloads.wkhtmltopdf.org/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 sudo apt-get install -f -y
 sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
 sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
-
-#--------------------------------------------------
-# Install ODOO
-#--------------------------------------------------
-
-echo -e "\n---- Create custom module directory ----"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom/addons"
-
-echo -e "\n---- Setting permissions on home folder ----"
-sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
-
-echo -e "\n---- Install python packages ----"
-sudo easy_install pyPdf vatnumber pydot psycogreen suds ofxparse
 
 
 
@@ -101,7 +45,7 @@ sudo easy_install pyPdf vatnumber pydot psycogreen suds ofxparse
 # Configure ODOO
 #--------------------------------------------------
 echo -e "* Create server config file"
-sudo cp $OE_HOME_EXT/debian/odoo.conf /etc/$OE_CONFIG.conf
+sudo cp $OE_HOME/debian/odoo.conf /etc/$OE_CONFIG.conf
 sudo chown $OE_USER:$OE_USER /etc/$OE_CONFIG.conf
 sudo chmod 640 /etc/$OE_CONFIG.conf
 
