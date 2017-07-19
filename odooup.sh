14c1d4608d9cf15de51ab0fec510cef9f8147927
Bessam Djerad <bessam.dj@gmail.com>
2017-07-19 09:24:02 +0100

    Update odooup.sh

--- odooup.sh
+++ odooup.sh
@@ -26,24 +26,6 @@ OE_CONFIG="$OE_USER"
 
 
 
-#--------------------------------------------------
-# Install PostgreSQL Server
-#--------------------------------------------------
-
-echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
-
-sudo service postgresql restart
-
-#--------------------------------------------------
-# System Settings
-#--------------------------------------------------
-
-echo -e "\n---- Create ODOO system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
-sudo adduser $OE_USER sudo
-
-echo -e "\n---- Create Log directory ----"
 sudo mkdir /var/log/$OE_USER
 sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 
