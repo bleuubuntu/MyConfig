cb9b98bcbcef6374697c58187006a71b41a1f157
Bessam Djerad <bessam.dj@gmail.com>
2018-05-06 14:42:37 +0200

    Update odoo11.sh

--- odoo11.sh
+++ odoo11.sh
@@ -36,7 +36,7 @@ OE_CONFIG="$OE_USER"
 
 echo -e "\n---- Install PostgreSQL 10 Serv ----"
 # sudo apt-get install postgresql -y
-echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' >> /etc/apt/sources.list.d/pgdg.list
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main' >> /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 sudo apt-get update
