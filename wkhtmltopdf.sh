1cb82f966cb4a172bd983b3919322013a48e23bf
Bessam Djerad <bessam.dj@gmail.com>
2018-05-28 15:28:17 +0200

    Update wkhtmltopdf.sh

--- wkhtmltopdf.sh
+++ wkhtmltopdf.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-apt remove wkhtmltopdf
-apt --fix-broken install
-wget http://ftp.br.debian.org/debian/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_amd64.deb
-dpkg -i libjpeg62-turbo_1.5.1-2_amd64.deb
 
- wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
-dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
-wget https://nightly.odoo.com/extra/wkhtmltox-0.12.1.2_linux-jessie-amd64.deb
-dpkg -i wkhtmltox-0.12.1.2_linux-jessie-amd64.deb
-apt install -f -y
+sudo wget http://downloads.wkhtmltopdf.org/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb --no-check-certificate
+sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+sudo apt-get install -f -y
+sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
+sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
