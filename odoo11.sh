7c4fdfa5d2f60e6b81a84e2711dbc83fac252712
Bessam Djerad <bessam.dj@gmail.com>
2020-12-14 12:40:38 +0100

    Update odoo11.sh

--- odoo11.sh
+++ odoo11.sh
@@ -136,7 +136,8 @@ dpkg -i libjpeg62-turbo_1.5.1-2_amd64.deb
  wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
 dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
 
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.debsudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 apt-get install -f -y
 
 sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
